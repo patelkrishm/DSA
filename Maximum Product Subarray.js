0e7f0a68eb18aab0a37e8726f7a84260101d3376
patelkrishm <krispatel074@gmail.com>
2025-11-27 09:12:06 +0530

    Enhance Maximum Product Subarray with comments
    Added comments and examples for clarity.

--- Maximum Product Subarray.js
+++ Maximum Product Subarray.js
@@ -1,4 +1,18 @@
-// 
+// Maximum Product Subarray
+// Given an integer array nums, find a subarray that has the largest product, and return the product.
+// The test cases are generated so that the answer will fit in a 32-bit integer.
+// Note that the product of an array with a single element is the value of that element
+
+// Example 1:
+// Input: nums = [2,3,-2,4]
+// Output: 6
+// Explanation: [2,3] has the largest product 6.
+
+// Example 2:
+// Input: nums = [-2,0,-1]
+// Output: 0
+// Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
 
 var maxProduct = function(nums) {
     let max = nums[0]
@@ -18,4 +32,5 @@ var maxProduct = function(nums) {
 };
 
 console.log(maxProduct([2, 3, -2, 4]))
+
 console.log(maxProduct([-2, 0, -1]))
