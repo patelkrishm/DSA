d2632806316c53a278ac37564eaf75226a901580
patelkrishm <krispatel074@gmail.com>
2025-12-01 09:29:10 +0530

    Add files via upload

---
+++ 3Sum.js
@@ -0,0 +1,43 @@
+// 3Sum
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Example 1:
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+
+// Example 2:
+// Input: nums = [0,1,1]
+// Output: []
+
+var threeSum = function(nums) {
+    let res = []
+    nums.sort((a, b) => a - b)
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            let sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                res.push([nums[i], nums[left], nums[right]])
+                left++
+                right--
+                while (left < right && nums[left] === nums[left - 1]) left++;
+                while (left < right && nums[right] === nums[right + 1]) right--;
+            } else if (sum < 0) {
+                left++
+            } else {
+                right--
+            }
+        }
+    }
+    return res
+};
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]))
+console.log(threeSum([0, 1, 1]))
